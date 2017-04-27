0494eb3e8f08620dedeb7d43da7b821ad254502f
David <david_elvir@unitec.edu>
2017-04-27 09:54:42 -0600

    primer dia arreglos

---
+++ arreglos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int size = 5;
+    int arreglo1[size];
+    
+    int arreglo2[]={10,20,30,40,50};
+    char diasSemana[]={'L','M','m','J','V','S','D'};
+    
+    //matrices
+    int matriz[5][5];
+    int cubo[5][5][5];
+return 0;
+}
